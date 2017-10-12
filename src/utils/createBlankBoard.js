07ba9fbd0df638d4d29a53e76b12f8a95066961a
Christian <ChrisWcs@users.noreply.github.com>
2017-10-11 22:43:26 -0600

    Create createBlankBoard function

---
+++ src/utils/createBlankBoard.js
@@ -0,0 +1,12 @@
+const createBlankBoard = (r, c) => {
+    let arr = [];
+
+    for(let i = 0; i < r; i++){
+        arr[i] = [];
+        for(let j = 0; j < c; j++){
+            arr[i][j] = '';
+        }
+    }
+
+    return arr;
+}
