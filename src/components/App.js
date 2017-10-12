db80a42473ba446ee7609619b83554bfe47da161
Christian <ChrisWcs@users.noreply.github.com>
2017-10-11 22:39:39 -0600

    Demo is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import Board from './board/Board';
 
 class App extends Component {
+    constructor(props){
+        super(props);
 
+        this.state = {
+            mat: [
+                ['h', 'b', 'b'],
+                ['', '', 'b'],
+                ['', '', ''],
+            ]
+        }
+    }
+
+
+    render(){
+        return(
+            <Board arr={this.state.mat}/>
+        );
+    }
 }
 
 export default App;
